f94f3af1f3c25402f9eb019cf9afafc500e94545
YangChao <1219191915@qq.com>
2025-09-18 22:36:46 +0800

    add: init project

---
+++ shuyou-core/src/main/java/com/shuyoutech/common/core/util/DataSizeUtils.java
@@ -0,0 +1,47 @@
+package com.shuyoutech.common.core.util;
+
+import cn.hutool.core.io.unit.DataSizeUtil;
+
+/**
+ * @author YangChao
+ * @date 2025-09-18 22:29
+ **/
+public class DataSizeUtils extends DataSizeUtil {
+
+    /**
+     * 格式化上下文
+     *
+     * @param size 大小
+     * @return 格式化
+     */
+    public static String formatContextWindow(long size) {
+        if (size == 1024) {
+            return "1K";
+        } else if (size == 2048) {
+            return "2K";
+        } else if (size == 4096) {
+            return "4K";
+        } else if (size == 8192) {
+            return "8K";
+        } else if (size == 16384) {
+            return "16K";
+        } else if (size == 32768) {
+            return "32K";
+        } else if (size == 65536) {
+            return "64K";
+        } else if (size == 131072) {
+            return "128K";
+        } else if (size == 262144) {
+            return "256K";
+        } else if (size == 400000) {
+            return "400K";
+        } else if (size == 524288) {
+            return "512K";
+        } else if (size == 10000000) {
+            return "1M";
+        } else {
+            return String.valueOf(size);
+        }
+    }
+
+}
---
+++ shuyou-core/src/main/java/com/shuyoutech/common/core/util/DesensitizedUtils.java
@@ -0,0 +1,29 @@
+package com.shuyoutech.common.core.util;
+
+import cn.hutool.core.util.DesensitizedUtil;
+import cn.hutool.core.util.StrUtil;
+
+/**
+ * @author YangChao
+ * @date 2025-09-18 14:03
+ **/
+public class DesensitizedUtils extends DesensitizedUtil {
+
+    /**
+     * 显示首尾API KEY
+     *
+     * @param apiKey 密钥
+     * @return 脱敏后的key
+     */
+    public static String apiKey(String apiKey) {
+        if (StringUtils.isBlank(apiKey)) {
+            return StrUtil.EMPTY;
+        }
+        int length = apiKey.length();
+        if (length < 10) {
+            return "********************";
+        }
+        return apiKey.substring(0, 7) + "**********" + apiKey.substring(length - 3);
+    }
+
+}
